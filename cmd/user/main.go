039727162e056c6b9f7ef418e9199d58f349d566
wicaker <wicaksanaigede@gmail.com>
2020-07-17 21:07:12 +0800

    first init

---
+++ cmd/user/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Print(err)
+	}
+}
+
+func main() {}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/wicaker/user
+
+go 1.14
+
+require (
+	github.com/golang-migrate/migrate/v4 v4.11.0
+	github.com/joho/godotenv v1.3.0
+	github.com/lib/pq v1.7.0
+)
