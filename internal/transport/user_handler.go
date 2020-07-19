ed65873e2a1199acf3092024b50c7881bd4ecebc
wicaker <wicaksanaigede@gmail.com>
2020-07-19 21:55:51 +0800

    add register handler

--- internal/transport/user_handler.go
+++ internal/transport/user_handler.go
@@ -1,9 +1,15 @@
 package transport
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
 	"github.com/labstack/echo"
 
 	"github.com/wicaker/user/internal/domain"
+	"github.com/wicaker/user/internal/middleware"
 	"github.com/wicaker/user/internal/pkg/rmq"
 )
 
@@ -44,7 +50,34 @@ func NewUserHandler(e *echo.Echo, rmqQueue []rmq.Queue, u domain.UserUsecase) {
 
 // Register will handle register request
 func (uh *UserHandler) Register(c echo.Context) error {
-	return nil
+	var user domain.User
+
+	err := c.Bind(&user)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, domain.Response{Message: err.Error()})
+	}
+
+	if ok, err := middleware.Validate(&user); !ok {
+		return c.JSON(http.StatusBadRequest, domain.Response{Message: "Validation error", Errors: err})
+	}
+
+	ctx := c.Request().Context()
+	if ctx == nil {
+		ctx = context.Background()
+	}
+
+	token, err := uh.UserUsecase.Register(ctx, &user)
+	if err != nil {
+		return c.JSON(domain.GetStatusCode(err), domain.Response{Message: err.Error()})
+	}
+
+	rabbitMessage := fmt.Sprintf(`{"email_destination":"%s","token":"%s"}`, user.Email, token)
+	err = uh.queuePublishRegister.Publish(rabbitMessage, "user.register", make(map[string]interface{}))
+	if err != nil {
+		log.Println(err)
+	}
+
+	return c.JSON(http.StatusCreated, domain.Response{Message: "Successfully register new user. Please confirm your email address!"})
 }
 
 // Login will handle login request
