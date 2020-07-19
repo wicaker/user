fd51fb38e06507020ea5f873c23b8b94a79e2762
wicaker <wicaksanaigede@gmail.com>
2020-07-19 21:57:11 +0800

    add some test configuration (echo, rmq, etc...)

--- test/integration/integration_test.go
+++ test/integration/integration_test.go
@@ -1,21 +1,39 @@
 package integration_test
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 	"testing"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/golang-migrate/migrate"
 	"github.com/jmoiron/sqlx"
+	"github.com/labstack/echo"
 
 	"github.com/wicaker/user/config"
+	"github.com/wicaker/user/internal/domain"
+	"github.com/wicaker/user/internal/pkg/rmq"
+	"github.com/wicaker/user/internal/repository"
+	"github.com/wicaker/user/internal/transport"
 	"github.com/wicaker/user/test/dbfixture"
+	"github.com/wicaker/user/test/mock"
 )
 
+type messageInMq struct {
+	EmailDestination string `json:"email_destination"`
+	Token            string `json:"token"`
+}
+
 var (
-	dbConn       *sqlx.DB
-	migrate_down string
+	dbConn           *sqlx.DB
+	migrate_down     string
+	api              *echo.Echo
+	publishedMessage mock.Message
+	listrmq          []rmq.Queue
 )
 
 func init() {
@@ -35,6 +53,12 @@ func TestMain(m *testing.M) {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// registering queue or channel
+	registerMockQueue()
+
+	// registering transport
+	api = transport.Echo(dbConn, listrmq)
 
 	// migrate up
 	err = sqlxConf.MigrateUp("file://../../migrations")
@@ -66,4 +90,55 @@ func TestMain(m *testing.M) {
 	}
 
 	os.Exit(code)
+}
+
+func getMessageInMq() messageInMq {
+	defer func() {
+		publishedMessage = mock.Message{}
+	}()
+
+	message := messageInMq{}
+
+	if publishedMessage.Message != "" {
+		err := json.Unmarshal([]byte(publishedMessage.Message), &message)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	return message
+}
+
+func createJWT(user domain.User, exp time.Duration) string {
+	expiresAt := time.Now().Add(exp).Unix()
+	tk := &domain.JWToken{
+		UUID:  user.UUID,
+		Email: user.Email,
+		Salt:  user.Salt,
+		StandardClaims: &jwt.StandardClaims{
+			ExpiresAt: expiresAt,
+		},
+	}
+	token := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), tk)
+	tokenString, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return tokenString
+}
+
+func makeUserActive(user *domain.User) error {
+	userRepo := repository.NewUserSqlxRepository(dbConn)
+
+	user.IsActive = true
+
+	_, err := userRepo.Update(context.TODO(), user)
+	return err
+}
+
+func registerMockQueue() {
+	listrmq = append(listrmq, mock.NewMockQueueRMQ("publish-user-register", &publishedMessage))
+	listrmq = append(listrmq, mock.NewMockQueueRMQ("publish-user-change-password", &publishedMessage))
+	listrmq = append(listrmq, mock.NewMockQueueRMQ("publish-user-forgot-password", &publishedMessage))
 }
