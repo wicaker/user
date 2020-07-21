84b4133044af35d110302cd9a7b3a6665762ff1b
wicaker <wicaksanaigede@gmail.com>
2020-07-21 20:04:19 +0800

    add SeedActiveUsers

--- test/dbfixture/user_data_fixtures.go
+++ test/dbfixture/user_data_fixtures.go
@@ -48,3 +48,42 @@ func SeedUsers(dbConn *sqlx.DB, count int) ([]domain.User, error) {
 
 	return users, nil
 }
+
+// SeedActiveUsers handles seeding the users table in the database for integration tests
+func SeedActiveUsers(dbConn *sqlx.DB, count int) ([]domain.User, error) {
+	var users []domain.User
+
+	for i := 1; i <= count; i++ {
+		user := domain.User{
+			Email:    fmt.Sprintf("user%d@example.com", i),
+			Password: fmt.Sprintf("Password%d", i),
+		}
+
+		// hash password
+		password, _ := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+		user.Password = string(password)
+
+		stmt, err := dbConn.Prepare("INSERT INTO users (email, password, is_active) VALUES ($1, $2, $3) RETURNING uuid, created_at, updated_at")
+		if err != nil {
+			return nil, errors.Wrap(err, "prepare users insertion")
+		}
+
+		row := stmt.QueryRow(user.Email, user.Password, true)
+
+		if err = row.Scan(&user.UUID, &user.CreatedAt, &user.UpdatedAt); err != nil {
+			if err := stmt.Close(); err != nil {
+				return nil, errors.Wrap(err, "close psql statement")
+			}
+
+			return nil, errors.Wrap(err, "capture users id")
+		}
+
+		if err := stmt.Close(); err != nil {
+			return nil, errors.Wrap(err, "close psql statement")
+		}
+
+		users = append(users, user)
+	}
+
+	return users, nil
+}
