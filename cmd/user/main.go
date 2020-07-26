7e8a29308318c114b94ae9b96ca9479d726aefb7
wicaker <wicaksanaigede@gmail.com>
2020-07-26 14:06:15 +0800

    set up main.go, etc...

--- cmd/user/main.go
+++ cmd/user/main.go
@@ -1,19 +1,121 @@
 package main
 
 import (
-	"log"
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
-	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
+
+	"github.com/wicaker/user/config"
+	"github.com/wicaker/user/internal/transport"
 )
 
 func init() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Print(err)
+		logrus.WithFields(logrus.Fields{
+			"at": time.Now().Format("2006-01-02 15:04:05"),
+		}).Println(err)
 	}
 }
 
-func main() {}
+func main() {
+	var (
+		sqlxConf   = config.NewSqlx()
+		dbConn     *sqlx.DB
+		rabbitConn = config.NewRabbitmq()
+		errChan    = make(chan error)
+	)
+	defer close(errChan)
+	defer func() {
+		err := rabbitConn.Close()
+		if err != nil {
+			logrus.WithFields(logrus.Fields{
+				"at": time.Now().Format("2006-01-02 15:04:05"),
+			}).Errorln(err)
+		}
+	}()
+	defer func() {
+		err := sqlxConf.Close(dbConn)
+		if err != nil {
+			logrus.WithFields(logrus.Fields{
+				"at": time.Now().Format("2006-01-02 15:04:05"),
+			}).Errorln(err)
+		}
+	}()
+
+	// open db connection
+	dbConn, err := sqlxConf.Open()
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"at": time.Now().Format("2006-01-02 15:04:05"),
+		}).Fatal(err)
+	}
+
+	go func() {
+		for {
+			errServerClosed := make(chan error)
+			eServer := transport.Echo(dbConn, rabbitConn.Queue)
+			srv := &http.Server{
+				Addr:         ":" + os.Getenv("SERVER_ECHO_PORT"),
+				WriteTimeout: 15 * time.Second,
+				ReadTimeout:  15 * time.Second,
+				Handler:      eServer,
+			}
+			logrus.WithFields(logrus.Fields{
+				"at": time.Now().Format("2006-01-02 15:04:05"),
+			}).Printf("Starting echo server on port :%s...", os.Getenv("SERVER_ECHO_PORT"))
+
+			// start http server
+			go func() {
+				err := srv.ListenAndServe()
+				if err != nil {
+					if err == http.ErrServerClosed {
+						errServerClosed <- err
+					} else {
+						errChan <- err
+					}
+				}
+			}()
+
+			// reconnect when rabbitmq server terminated accidentally
+			err = <-rabbitConn.ErrorChannel
+			if !rabbitConn.IsClose {
+				rabbitConn.Reconnect(err)
+			} else {
+				errChan <- err
+			}
+
+			// shutdown http server gracefully
+			ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+			defer cancel()
+			if err := srv.Shutdown(ctx); err != nil {
+				errChan <- err
+			}
+
+			logrus.WithFields(logrus.Fields{
+				"at": time.Now().Format("2006-01-02 15:04:05"),
+			}).Println(<-errServerClosed)
+		}
+	}()
+
+	go func() {
+		c := make(chan os.Signal)
+		signal.Notify(c, os.Interrupt)
+		errChan <- fmt.Errorf("%s", <-c)
+	}()
+
+	logrus.WithFields(logrus.Fields{
+		"at": time.Now().Format("2006-01-02 15:04:05"),
+	}).Errorln("terminated ", <-errChan)
+}
--- go.mod
+++ go.mod
@@ -10,8 +10,7 @@ require (
 	github.com/google/uuid v1.1.1
 	github.com/jmoiron/sqlx v1.2.0
 	github.com/joho/godotenv v1.3.0
-	github.com/labstack/echo v3.3.10+incompatible
-	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/labstack/echo/v4 v4.1.16
 	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/lib/pq v1.7.0
 	github.com/pkg/errors v0.9.1
@@ -19,5 +18,6 @@ require (
 	github.com/streadway/amqp v1.0.0
 	github.com/stretchr/testify v1.4.0
 	golang.org/x/crypto v0.0.0-20200302210943-78000ba7a073
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	gopkg.in/go-playground/validator.v9 v9.31.0
 )
